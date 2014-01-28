8d8d6310a37586a6e399a3db90570c84a6ddecf0
Alvin Ang <alvinangbs@gmail.com>
2014-01-27 17:41:40 -0800

    updated test-first-ruby

--- w1/w1d1/Test-First-Ruby/in_words.rb
+++ w1/w1d1/Test-First-Ruby/in_words.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 class Fixnum
 
   def in_words
@@ -22,9 +23,9 @@ class Fixnum
 
     # replicate self so true value remains
     dup = self
-    counter = 1
-
+    counter = 0
     # loop until self is less than 1000
+
     while dup > 1000
       to_check = dup % 1000
 
@@ -42,26 +43,36 @@ class Fixnum
       end
 
       # add fillers
-      result << 'and'
-      result << fillers[0]
+      # result << 'and'
+
+      result << fillers[0] unless hundred_check == 0
       result << integers[hundred_check % 10] unless (hundred_check % 10 == 0)
-      result << fillers[counter]
+      result << fillers[counter] unless to_check / 100 == 0
+
       counter += 1
       dup = dup / 1000
     end
 
+
     # for the final 3 numbers after dividing by 1000
-    if dup < 10
+    if dup <= 10
+      result << fillers[counter]
       result << integers[dup]
     elsif dup < 20
+      result << fillers[counter]
       result << teens[dup - 11]
     elsif dup < 100
       result << integers[dup % 10] unless (dup % 10 == 0)
       result << tens[(dup / 10) - 1]
-    else dup < 1000
-      result << integers[(dup / 1) % 10] unless ((dup / 1) % 10 == 0)
-      result << tens[(dup / 10) % 10 - 1] unless ((dup / 10) % 10 == 0)
-      result << 'and'
+    else dup <= 1000
+      result << 'thousand' if dup == 1000
+      if (dup % 100) < 20 && (dup % 100 > 10)
+        result << teens[(dup % 100) - 11]
+      else
+        result << integers[(dup / 1) % 10] unless ((dup / 1) % 10 == 0)
+        result << tens[(dup / 10) % 10 - 1] unless ((dup / 10) % 10 == 0)
+      end
+      # result << 'and'
       result << fillers[0]
       result << integers[(dup / 100) % 10] unless ((dup / 100) % 10 == 0)
     end
