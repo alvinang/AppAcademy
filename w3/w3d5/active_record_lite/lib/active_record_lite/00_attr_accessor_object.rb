af60b85e02465bf160c0c57e1a19e68bba913811
Alvin Ang <alvinangbs@gmail.com>
2014-02-14 10:49:19 -0800

    complete attr_accessor;

--- w3/w3d5/active_record_lite/lib/active_record_lite/00_attr_accessor_object.rb
+++ w3/w3d5/active_record_lite/lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,23 @@
+# def name=(name)
+#   @name = name
+# end
+# 
+# def name
+#   @name
+# end
+# define_method takes a string
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    
+    names.each do |name|
+      define_method("#{name.to_s}=") do |arg| 
+        instance_variable_set("@#{name}", arg)
+      end
+    
+      define_method(name.to_s) do 
+        instance_variable_get("@#{name}")
+      end
+    end
   end
 end
