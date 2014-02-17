53a960dd11b44bf4db117c3ed8c4b1f6c6efb578
Alvin Ang <alvinangbs@gmail.com>
2014-02-16 20:26:54 -0800

    Initial commit for blogger app

---
+++ w3/w3d7/README.md
@@ -0,0 +1,2 @@
+## Blogger from JumpStartLab
+This app is a tutorial from JumpStartLab [Blogger](http://tutorials.jumpstartlab.com/projects/blogger.html).
---
+++ w3/w3d7/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '64bc675e18b7337eb597c2fbfe850b6787a093bbaad3271ff1e5b4dcfd3b42037329ef73a30e7eb70c263161d8e684ea96b957768d8787b4617edff17c89e3f3'
