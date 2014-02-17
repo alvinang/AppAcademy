5c79017325532ecf883e35f14e704de924e9c255
Alvin Ang <alvinangbs@gmail.com>
2014-02-16 21:00:45 -0800

    add index and create view and controllers

--- w3/w3d7/blogger/db/seeds.rb
+++ w3/w3d7/blogger/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Article.create!(title: 'Sample Article Title', body: 'This is the text for my article')
+a2 = Article.create!(title: 'Title2', body: 'Text2')
+a3 = Article.create!(title: 'Title3', body: 'Test3')
