3222f749f81b7f2a2930967665ebaf6f600dec40
Alvin Ang <alvinangbs@gmail.com>
2014-02-22 18:52:51 -0800

    Complete Hartl C5

--- w4/w4d6/hartl-tutorial/sample_app/spec/requests/static_pages_spec.rb
+++ w4/w4d6/hartl-tutorial/sample_app/spec/requests/static_pages_spec.rb
@@ -1,48 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
   
   describe "Home page" do
+    before { visit root_url }
     
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end    
-    
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')      
-      # can also do substrings. i.e. .to have_title('Home')
-    end
-    
+    it { should have_content 'Sample App' }
+    it { should have_title full_title('') }
+    it { should_not have_title '| Home' }    
   end
   
   describe "Help page" do
+    before { visit help_url }
     
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-    
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title('Help')
-    end
+    it { should have_content 'Help' }
+    it { should have_title full_title('Help') }
   end
   
   describe "About page" do
+    before { visit about_url }
     
-    it "should have the content 'About us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
-    
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title('About')
-    end    
-    
+    it { should have_content 'About' }
+    it { should have_title full_title('About') }    
   end
   
+  describe "Contact page" do 
+    before { visit contact_url }
     
+    it { should have_content 'Contact' }
+    it { should have_title full_title('Contact') }    
+  end
 end
---
+++ w4/w4d6/hartl-tutorial/sample_app/spec/requests/user_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "UserPages" do
+
+  subject { page }
+  
+  describe "signup page" do 
+    before { visit signup_url }
+    
+    it { should have_content 'Sign up' }
+    it { should have_title full_title('Sign up') }
+  end
+
+end
