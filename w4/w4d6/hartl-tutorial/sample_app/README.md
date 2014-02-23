d7b9ddf7d609ac7dad9b3af761890be0a8f2c0b4
Alvin Ang <alvinangbs@gmail.com>
2014-02-22 20:48:38 -0800

    Make a basic user model (including secure password)

--- w4/w4d6/hartl-tutorial/sample_app/README.md
+++ w4/w4d6/hartl-tutorial/sample_app/README.md
@@ -3,3 +3,27 @@
 This is the sample application for
 the [*Ruby on Rails Tutorial*](http://railstutorial.org/)
 by [Michael Hartl](http://michaelhartl.com/).
+
+* if you get a deprecation warning like:
+```ruby
+[deprecated] I18n.enforce_available_locales will default to true in the future
+```
+
+- get it of it by editing config/application.rb:
+```ruby
+require File.expand_path('../boot', __FILE__)
+.
+.
+.
+module SampleApp
+  class Application < Rails::Application
+    .
+    .
+    .
+    I18n.enforce_available_locales = true
+    .
+    .
+    .
+  end
+end
+```
---
+++ w4/w4d6/hartl-tutorial/sample_app/app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+             uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+             
+  before_save { self.email = email.downcase }
+  has_secure_password
+  
+end
