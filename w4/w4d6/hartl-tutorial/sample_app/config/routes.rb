ded3106b28c6e20041dfce6a34a9e5ec25f254a0
Alvin Ang <alvinangbs@gmail.com>
2014-02-22 23:25:18 -0800

    Complete Ch 7 Hartl

--- w4/w4d6/hartl-tutorial/sample_app/config/routes.rb
+++ w4/w4d6/hartl-tutorial/sample_app/config/routes.rb
@@ -1,7 +1,7 @@
-SampleApp::Application.routes.draw do
-  get "users/new"
-  
+SampleApp::Application.routes.draw do  
   root 'static_pages#home'
+  resources :users
+  
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
---
+++ w4/w4d6/hartl-tutorial/sample_app/spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  'Mike Wilson'
+    email                 'mike@example.com'
+    password              'foobar'
+    password_confirmation 'foobar'
+  end
+end
